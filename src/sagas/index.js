50034bde4ee39047c45f95462c01c11082c49669
ZoeLeBlanc <zgleblanc@gmail.com>
2019-01-29 10:45:12 -0500

    fixed record delete

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -318,6 +318,7 @@ function* fetchRecordsBySlug(action) {
 
 
 function* fetchRecords(action) {
+	console.log('featch', action)
 	yield put({ type: ACTION_TYPE.RECORDS_LOADING, payload: true});
 
 	try {
