fb31cd2607153feaf10c731771ce2e01f567eba1
ZoeLeBlanc <zgleblanc@gmail.com>
2019-01-31 16:25:41 -0500

    pretty close to v1

--- src/views/exhibits/show.js
+++ src/views/exhibits/show.js
@@ -63,7 +63,6 @@ class ExhibitShow extends Component {
 				this.props.fetchRecordsBySlug(this.props.match.params.slug);
 			}
 		if (this.props.filteredRecords !== prevProps.filteredRecords){
-			console.log('change in filtered records')
 			this.setState({records: this.props.filteredRecords});
 			this.props.dispatch(this.props.updateRecordCache(this.props.filteredRecords));
 		}
